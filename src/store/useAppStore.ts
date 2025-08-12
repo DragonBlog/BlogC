4c180b32e31cecff9b5b0304a0896506ca11cd43
yexiyue <yexiyue666@qq.com>
2025-08-12 11:14:23 +0800

    docs: 添加产品分析

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -6,14 +6,18 @@ import { TauriStoreState } from "./tauriStoreState";
 const store = new TauriStoreState("app-store.json");
 await store.init();
 
+type Theme = "light" | "dark" | "system";
+
 export const useAppStore = createWithEqualityFn(
   persist(
     combine(
       {
         accessToken: "",
+        theme: "system" as Theme,
       },
       (set) => ({
         setAccessToken: (token: string) => set({ accessToken: token }),
+        setTheme: (theme: Theme) => set({ theme }),
       }),
     ),
     {
